b6b1253f2145771c925bb9c70698bca0ee5c8ce1
Даниил Демидко <DanielDemidko@gmail.com>
2017-05-01 19:31:11 +1000

    Add files via upload

---
+++ src/Extensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+
+static class Extensions
+{
+    public static String GetDomainName(this String url)
+    {
+        // Порядок префиксов важен
+        foreach (String i in new[] { "https://", "http://", "www." })
+        {
+            if (url.IndexOf(i) == 0)
+            {
+                url = url.Remove(0, i.Length);
+            }
+        }
+        Int32 subdomain = url.IndexOf('/');
+        return subdomain == -1 ? url : url.Remove(subdomain);
+    }
+
+    public static List<String> GetHtmlLinks(this String page, String domainName = null)
+    {
+        List<String> result = new List<String>();
+        Regex reHref = new Regex(@"(?inx)
+        <a \s [^>]*
+            href \s* = \s*
+                (?<q> ['""] )
+                    (?<url> [^""]+ )
+                \k<q>
+        [^>]* >");
+        foreach (Match i in reHref.Matches(page))
+        {
+            result.Add(i.Groups["url"].ToString());
+        }
+        return domainName == null ? result : new List<String>(result.Where(i => i.GetDomainName() == domainName));
+    }
+}
+
---
+++ src/RandomBrowser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+class RandomBrowser
+{
+    private readonly WebClient Downoader = new WebClient();
+    private readonly Random Randomizer = new Random();
+
+    public readonly String DomainName;
+    public readonly String DomainPage;
+    public String NextUrl { private set; get; }
+
+    public void Request()
+    {
+        try
+        {
+            List<String> links = Downoader.DownloadString(NextUrl).GetHtmlLinks(DomainName);
+            NextUrl = links.Count > 0 ? links[Randomizer.Next(links.Count)] : DomainPage;
+        }
+        catch (Exception)
+        {
+            NextUrl = DomainPage;
+        }
+    }
+
+    public RandomBrowser(String startPage)
+    {
+        DomainName = startPage?.GetDomainName();
+        DomainPage = @"http://www." + DomainName;
+        NextUrl = startPage;
+    }
+}
